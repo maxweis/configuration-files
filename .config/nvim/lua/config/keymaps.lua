690e1a802f3070b3e3a86994d8ad474dc8948427
mweislumen <maxon.weis@lumen.com>
2025-12-21 17:10:24 -0600

    Add switch to tab with <Leader + tab number>

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,3 +11,10 @@ map("n", "<C-S-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Wid
 map("n", "<C-S-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 map("n", "<C-S-k>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
 map("n", "<C-S-j>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+
+-- Navigate buffers using <leader> + number
+for i = 1, 9 do
+  map("n", "<leader>" .. i, function()
+    require("bufferline").go_to(i, true)
+  end, { desc = "Go to buffer " .. i })
+end
